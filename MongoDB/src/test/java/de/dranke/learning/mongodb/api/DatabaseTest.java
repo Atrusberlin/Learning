fdde526dccdce1a365cc75e77dfe5bd5f31d5afc
AtrusberlinHP <atrusberlin@gmail.com>
2012-03-30 08:21:23 +0200

    MongoTests erweitert

---
+++ MongoDB/src/test/java/de/dranke/learning/mongodb/api/DatabaseTest.java
@@ -0,0 +1,6 @@
+package de.dranke.learning.mongodb.api;
+
+/** Created by IntelliJ IDEA. User: daniel.ranke Date: 29.03.12 Time: 16:32 To change this template use File | Settings | File Templates. */
+public class DatabaseTest {
+
+}
