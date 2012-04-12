b3105a8f82c30226b7a8126d13c9c8c8c179d793
AtrusberlinHP <atrusberlin@gmail.com>
2012-04-12 14:07:52 +0200

    Datenbanktest erweitert. erstellen von Datenbanken.

--- MongoDB/src/test/java/de/dranke/learning/mongodb/api/DatabaseTest.java
+++ MongoDB/src/test/java/de/dranke/learning/mongodb/api/DatabaseTest.java
@@ -1,5 +1,6 @@
 package de.dranke.learning.mongodb.api;
 
+import com.mongodb.DB;
 import com.mongodb.Mongo;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -12,6 +13,7 @@ import static org.fest.assertions.Assertions.assertThat;
 /** Created by IntelliJ IDEA. User: daniel.ranke Date: 29.03.12 Time: 16:32 To change this template use File | Settings | File Templates. */
 public class DatabaseTest {
 
+  public static final String LEARNDB = "learn";
   private String dbname;
   private String hostName;
   private int hostPort;
@@ -21,7 +23,7 @@ public class DatabaseTest {
   public void setUp() throws Exception {
     dbname = "learn";
     hostName = "localhost";
-    hostPort = 27001;
+    hostPort = 27017;
   }
 
   @AfterMethod
@@ -56,11 +58,53 @@ public class DatabaseTest {
   @Test
   public void erzeugen_einer_Verbindung_mit_Host_und_Port_Parameter() throws UnknownHostException {
     // given
-    mongoConnection = new Mongo(hostName);
+    mongoConnection = new Mongo(hostName, hostPort);
     // when
     showMongoDbDetails(mongoConnection);
     // then
     assertThat(mongoConnection.getVersion()).isNotEmpty();
+  }
+
+  @Test
+  public void verbinden_zu_Datenbank_learn() throws UnknownHostException {
+    // given
+    mongoConnection = new Mongo(hostName);
+    // when
+    DB learn = mongoConnection.getDB(LEARNDB);
+    // then
+    assertThat(learn.getStats()).isNotNull();
+    showDBStats(learn);
+  }
+
+  @Test
+  public void erstelle_und_loesche_Datenbank() throws UnknownHostException {
+    // given
+    mongoConnection = new Mongo(hostName);
+    // when
+    String neueTestDatenbankName = "neueTestDatenbank";
+    // neue Datenbank wird erstellt, wenn sie noch nicht vorhanden ist
+    DB testDatenbank = mongoConnection.getDB(neueTestDatenbankName);
+
+    assertThat(testDatenbank).isNotNull();
+    assertThat(testDatenbank.getStats().ok()).isTrue();
+    assertThat(mongoConnection.getDatabaseNames().contains(neueTestDatenbankName)).isTrue();
+    showDBStats(testDatenbank);
+
+    // die Datenbank wird gelöscht.
+//    mongoConnection.dropDatabase(neueTestDatenbankName);
+    // eine alternative Löschvariante ist
+    testDatenbank.dropDatabase();
+    assertThat(mongoConnection.getDatabaseNames().contains(neueTestDatenbankName)).isFalse();
+
+    // then
+
+  }
+
+  private void showDBStats(DB database) {
+    System.out.println("ok=" + database.getStats().ok());
+    for (String key : database.getStats().keySet()) {
+      System.out.println("Key(" + key + ")=" + database.getStats().get(key));
+    }
   }
 
   private void showMongoDbDetails(Mongo mongoConnection) {
