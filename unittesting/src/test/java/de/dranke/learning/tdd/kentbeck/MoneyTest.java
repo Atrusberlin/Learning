be5f77dfe398c4fc32ea91b6577c9c1f25e3a40f
Atrusberlin <atrusberlin@gmail.com>
2012-04-25 23:04:15 +0200

    [TDD KentBeck] Kapitel 11 abgeschlossen

--- unittesting/src/test/java/de/dranke/learning/tdd/kentbeck/MoneyTest.java
+++ unittesting/src/test/java/de/dranke/learning/tdd/kentbeck/MoneyTest.java
@@ -13,8 +13,6 @@ public class MoneyTest extends TestCase {
   public void testEquality() throws Exception {
     assertTrue(Money.dollar(5).equals(Money.dollar(5)));
     assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-    assertTrue(Money.franc(5).equals(Money.franc(5)));
-    assertFalse(Money.franc(5).equals(Money.franc(6)));
     assertFalse(Money.franc(5).equals(Money.dollar(5)));
   }
 
@@ -27,9 +25,5 @@ public class MoneyTest extends TestCase {
   public void testCurrency() {
     assertEquals("USD", Money.dollar(1).currency());
     assertEquals("CHF", Money.franc(1).currency());
-  }
-
-  public void testDifferentClassEquality() {
-    assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
   }
 }
