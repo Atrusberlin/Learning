e52de5ff8cb9003c111d393b1543ec7ceb5815b6
Atrusberlin <atrusberlin@gmail.com>
2012-04-25 22:04:26 +0200

    [TDD KentBeck] Kapitel 9 abgeschlossen

--- unittesting/src/test/java/de/dranke/learning/tdd/kentbeck/FrancTest.java
+++ unittesting/src/test/java/de/dranke/learning/tdd/kentbeck/FrancTest.java
@@ -12,11 +12,11 @@ import static junit.framework.Assert.assertEquals;
  * To change this template use File | Settings | File Templates.
  */
 public class FrancTest {
-  
+
   @Test
   public void testFrancMultiplication() {
-    Franc five = new Franc(5);
-    assertEquals(new Franc(10), five.times(2));
-    assertEquals(new Franc(15), five.times(3));
+    Money five = Money.franc(5);
+    assertEquals(Money.franc(10), five.times(2));
+    assertEquals(Money.franc(15), five.times(3));
   }
 }
