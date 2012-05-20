08eb4c413a60c9dab6034ab327307a58753e4f89
Atrusberlin <atrusberlin@gmail.com>
2012-05-20 14:20:33 +0200

    [TDD KentBeck] Kapitel 23 abgeschlossen (Python); xUnit abgeschlossen

--- unittesting/python/TestCaseTest.py
+++ unittesting/python/TestCaseTest.py
@@ -1,30 +1,50 @@
 from WasRun import WasRun
 from TestCase import *
+from TestSuite import *
 
 class TestCaseTest(TestCase):
+
+    def setUp(self):
+        self.result= TestResult()
         
     def testTemplateMethod(self):
         test= WasRun("testMethod")
-        test.run()
+        test.run(self.result)
         #print(self.test.log)
         assert("setUp testMethod tearDown " == test.log)
 
     def testResult(self):
         test= WasRun("testMethod")
-        result= test.run()
-        print(result.summary())
-        assert("1 run, 0 failed" == result.summary())
+        test.run(self.result)
+        #print(result.summary())
+        assert("1 run, 0 failed" == self.result.summary())
 
     def testFailedResult(self):
         test= WasRun("testBrokenMethod")
-        result= test.run()
+        test.run(self.result)
         assert("1 run, 1 failed", result.summary())
 
     def testFailedResultFormatting(self):
-        result= TestResult()
-        result.testStarted()
-        result.testFailed()
-        assert("1 run, 1 failed" == result.summary())
+        self.result.testStarted()
+        self.result.testFailed()
+        assert("1 run, 1 failed" == self.result.summary())
 
-TestCaseTest("testFailedResultFormatting").run()
-print("Test successful")
+    def testSuite(self):
+        suite= TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        suite.run(self.result)
+        assert("2 run, 1 failed" == self.result.summary())
+
+suite= TestSuite()
+suite.add(TestCaseTest("testTemplateMethod"))
+suite.add(TestCaseTest("testResult"))
+suite.add(TestCaseTest("testFailedResultFormatting"))
+suite.add(TestCaseTest("testFailedResult"))
+suite.add(TestCaseTest("testSuite"))
+result= TestResult()
+suite.run(result)
+print (result.summary())
+
+
+
---
+++ unittesting/python/TestSuite.py
@@ -0,0 +1,13 @@
+from TestCase import *
+
+class TestSuite:
+    def __init__(self):
+        self.tests= []
+
+    def add(self, test):
+        self.tests.append(test)
+
+    def run(self, result):
+        for test in self.tests:
+            test.run(result)
+        return result
