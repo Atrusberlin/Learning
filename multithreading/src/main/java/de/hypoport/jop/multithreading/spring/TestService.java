0b5e16ecadddf6db18ddc9f6e83d4cde118e50f9
Atrusberlin <atrusberlin@gmail.com>
2012-07-03 22:34:27 +0200

    [Multithreading]erster Async-Test

--- multithreading/src/main/java/de/hypoport/jop/multithreading/spring/TestService.java
+++ multithreading/src/main/java/de/hypoport/jop/multithreading/spring/TestService.java
@@ -1,19 +1,21 @@
 package de.hypoport.jop.multithreading.spring;
 
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 
-import java.util.Random;
 import java.util.concurrent.Future;
 
 @Service
 public class TestService {
 
   @Async
-  public Future<Integer> getRandomInteger(Integer min, Integer max) {
-    Random random = new Random();
-    random.nextInt(min.intValue());
+  public Future<Integer> getInteger(Integer wertFuerTimeOutInSekunden) throws InterruptedException {
+    long timeOut = wertFuerTimeOutInSekunden * 1000;
+    System.out.println("ThreadID: " + Thread.currentThread().getId() + " ;stop (" + timeOut + ")");
+    Thread.currentThread().sleep(timeOut);
+    System.out.println("ThreadID: " + Thread.currentThread().getId() + " wird fortgesetzt.");
 
-    return null;
+    return new AsyncResult<Integer>(wertFuerTimeOutInSekunden);
   }
 }
--- multithreading/src/test/java/de/hypoport/jop/multithreading/spring/TestServiceTest.java
+++ multithreading/src/test/java/de/hypoport/jop/multithreading/spring/TestServiceTest.java
@@ -1,7 +1,34 @@
 package de.hypoport.jop.multithreading.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+@ContextConfiguration("classpath:/spring-config/async-test.spring.xml")
 public class TestServiceTest extends AbstractTestNGSpringContextTests {
 
+  @Autowired
+  TestService testService;
+
+  @Test
+  public void timeOut_mit_einem_thread() throws ExecutionException, InterruptedException, TimeoutException {
+    // given
+    Future<Integer> future1 = testService.getInteger(4);
+
+    // when
+    Integer integer1 = future1.get(5, TimeUnit.SECONDS);
+    System.out.println("Zufallszahl1: " + integer1.toString());
+
+    // then
+    assertThat(integer1).isEqualTo(4);
+
+  }
 }
