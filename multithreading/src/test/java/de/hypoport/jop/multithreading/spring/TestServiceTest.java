ccff330f23525cbf5a3374c548bdaa7c18fe4238
Atrusberlin <atrusberlin@gmail.com>
2012-07-03 22:59:36 +0200

    [Multithreading] Kleine Ergänzung im Async-Test

--- multithreading/src/test/java/de/hypoport/jop/multithreading/spring/TestServiceTest.java
+++ multithreading/src/test/java/de/hypoport/jop/multithreading/spring/TestServiceTest.java
@@ -7,7 +7,6 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import static org.fest.assertions.Assertions.assertThat;
@@ -19,16 +18,35 @@ public class TestServiceTest extends AbstractTestNGSpringContextTests {
   TestService testService;
 
   @Test
-  public void timeOut_mit_einem_thread() throws ExecutionException, InterruptedException, TimeoutException {
+  public void timeOut_mit_einem_Thread() throws ExecutionException, InterruptedException, TimeoutException {
     // given
     Future<Integer> future1 = testService.getInteger(4);
 
     // when
-    Integer integer1 = future1.get(5, TimeUnit.SECONDS);
-    System.out.println("Zufallszahl1: " + integer1.toString());
+    Integer integer1 = future1.get();
+    System.out.println("Integer 1: " + integer1.toString());
 
     // then
     assertThat(integer1).isEqualTo(4);
+
+  }
+
+  @Test
+  public void timeOut_mit_zwei_Threads() throws ExecutionException, InterruptedException, TimeoutException {
+    // given
+    Future<Integer> future1 = testService.getInteger(4);
+    Future<Integer> future2 = testService.getInteger(7);
+
+    // when
+    System.out.println("Aufruf der Future.get()");
+    Integer integer1 = future1.get();
+    Integer integer2 = future2.get();
+    System.out.println("Integer 1: " + integer1.toString());
+    System.out.println("Integer 2: " + integer2.toString());
+
+    // then
+    assertThat(integer1).isEqualTo(4);
+    assertThat(integer2).isEqualTo(7);
 
   }
 }
