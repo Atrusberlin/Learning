0200c0a9889926ae814386203473ddd0902f6154
AtrusberlinHP <atrusberlin@gmail.com>
2013-02-06 13:04:20 +0100

    [MongoDB] Seltsames Verhalten bei der Authentifizierung

--- MongoDB/src/test/java/de/dranke/learning/mongodb/api/administration/AuthenticationTest.java
+++ MongoDB/src/test/java/de/dranke/learning/mongodb/api/administration/AuthenticationTest.java
@@ -1,9 +1,13 @@
 package de.dranke.learning.mongodb.api.administration;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
+import com.mongodb.DBCollection;
 import com.mongodb.Mongo;
 import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
+
+import java.net.UnknownHostException;
 
 import static org.fest.assertions.Assertions.assertThat;
 
@@ -19,5 +23,29 @@ public class AuthenticationTest {
     boolean authenticated = adminDb.authenticate("admin", "admin".toCharArray());
 
     assertThat(authenticated).isTrue();
+  }
+
+  @Test
+  public void authenticationContainsToConnection() throws UnknownHostException {
+    Mongo mongo = new Mongo();
+    DB adminDb = mongo.getDB("admin");
+
+    assertThat(adminDb.authenticate("admin", "admin".toCharArray())).isTrue();
+
+    DB newDb = mongo.getDB("newDb");
+//    assertThat(newDb.isAuthenticated()).isTrue();
+    DBCollection test1 = newDb.getCollection("test1");
+    test1.insert(new BasicDBObject("name", "Daniel"));
+    assertThat(test1.count()).isEqualTo(1);
+    test1.drop();
+    mongo.close();
+
+    Mongo mongoCon2 = new Mongo();
+    DB newDb1 = mongoCon2.getDB("newDb");
+    assertThat(mongoCon2.getDB("admin").isAuthenticated()).isFalse();
+    assertThat(newDb1.isAuthenticated()).isFalse();
+    test1 = newDb.getCollection("test1");
+    test1.insert(new BasicDBObject("name", "Daniel"));
+    test1.drop();
   }
 }
